16045596e38819e30576b134a9c4d599b04176d2
RJ Robinson <rj@localhost.localdomain>
2021-05-21 17:53:41 -0700

    props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
 import React from "react";
 import Navbar from "./components/layout/navbar.component";
+import UserItem from "./components/users/user-item.component";
 
 class App extends React.Component {
 
+  constructor() {
+    super();
+
+    this.state = {
+      id : "id",
+      login: "rj",
+      avatar_url:"https://avatars.githubusercontent.com/u/1?v=4",
+      html_url: "https://github.com/mojombo"
+
+    }
+  }
   
 
   render() {
@@ -11,9 +23,12 @@ class App extends React.Component {
       
 
     return (
+      <div className ="app">
       <nav className="bg-primary">
-        <Navbar/>
+        <Navbar title="GitHub Finder" icon="faGithub"/>
       </nav>
+      <UserItem/>
+      </div>
     );
   }
 
--- src/components/layout/navbar.component.jsx
+++ src/components/layout/navbar.component.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+
 
 export class Navbar extends Component {
     render() {
         return (
             <div>
-                <h1>Navbar</h1>
+                <h1>
+                <FontAwesomeIcon icon={faGithub} />
+                {this.props.title}</h1>
             </div>
         )
     }
